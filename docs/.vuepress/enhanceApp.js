1bcf0483c02893124032de9542b220ac176f7f0c
qiang <qw13131wang@gmail.com>
2019-09-03 15:40:35 +0800

    fix: styles

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -5,7 +5,12 @@ import {
   Message as ElMessage,
   Pagination as ElPagination
 } from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/base.css'
+import 'element-ui/lib/theme-chalk/button.css'
+import 'element-ui/lib/theme-chalk/upload.css'
+import 'element-ui/lib/theme-chalk/dialog.css'
+import 'element-ui/lib/theme-chalk/message.css'
+import 'element-ui/lib/theme-chalk/pagination.css'
 
 export default ({ Vue }) => {
   Vue.use(ElButton)
