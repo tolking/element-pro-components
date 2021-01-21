7118be5f89567c5909799666829d75b7208d0fc9
tolking <qw13131wang@gmail.com>
2021-01-21 14:48:23 +0800

    test: add test

--- test/inputTag.spec.ts
+++ test/inputTag.spec.ts
@@ -42,9 +42,9 @@ describe('InputTag.vue', () => {
     await wrapper.find('input').trigger('blur')
     expect(wrapper.find('input').element.value).toBe('')
     expect(getList(wrapper)).toContain('blur')
-    /** keyup.space */
+    /** keyup */
     await wrapper.find('input').setValue('space')
-    await wrapper.find('input').trigger('keyup.space')
+    await wrapper.find('input').trigger('keyup', { key: ' ' })
     expect(getList(wrapper)).toContain('space')
 
     /** close */
@@ -54,5 +54,86 @@ describe('InputTag.vue', () => {
     /** change model-value */
     await vm.value.push('model')
     expect(getList(wrapper)).toContain('model')
+  })
+
+  test('test props', async () => {
+    const wrapper = _mount({
+      template: `
+        <pro-input-tag
+          v-model="value"
+          :trigger="trigger"
+          :size="size"
+          :type="type"
+          :hit="hit"
+          :color="color"
+          :effect="effect"
+        />
+      `,
+      setup() {
+        const value = ref([])
+        const trigger = ref('space')
+        const size = ref('')
+        const type = ref('')
+        const hit = ref(false)
+        const color = ref('')
+        const effect = ref('light')
+
+        return {
+          value,
+          trigger,
+          size,
+          type,
+          hit,
+          color,
+          effect,
+        }
+      },
+    })
+    const vm = (wrapper.vm as unknown) as {
+      value: string[]
+      trigger: 'space' | 'enter'
+      size: 'medium' | 'small' | 'mini'
+      type: 'success' | 'info' | 'warning' | 'danger'
+      hit: boolean
+      color: string
+      effect: 'light' | 'dark' | 'plain'
+    }
+
+    // change trigger
+    await wrapper.find('input').setValue('space')
+    await wrapper.find('input').trigger('keyup', { key: ' ' })
+    expect(getList(wrapper)).toContain('space')
+    await (vm.trigger = 'enter')
+    await wrapper.find('input').setValue('enter')
+    await wrapper.find('input').trigger('keyup', { key: 'Enter' })
+    expect(getList(wrapper)).toContain('enter')
+
+    // change size
+    expect(wrapper.find('.el-tag').classes()).not.toContain('el-tag--small')
+    expect(wrapper.find('.el-input').classes()).not.toContain('el-input--small')
+    await (vm.size = 'small')
+    expect(wrapper.find('.el-tag').classes()).toContain('el-tag--small')
+    expect(wrapper.find('.el-input').classes()).toContain('el-input--small')
+
+    // change type
+    expect(wrapper.find('.el-tag').classes()).not.toContain('el-tag--danger')
+    await (vm.type = 'danger')
+    expect(wrapper.find('.el-tag').classes()).toContain('el-tag--danger')
+
+    // change hit
+    expect(wrapper.find('.el-tag').classes()).not.toContain('is-hit')
+    await (vm.hit = true)
+    expect(wrapper.find('.el-tag').classes()).toContain('is-hit')
+
+    // change color
+    await (vm.color = 'rgb(125, 233, 45)')
+    expect(wrapper.find('.el-tag').element.style.backgroundColor).toEqual(
+      'rgb(125, 233, 45)'
+    )
+
+    // change effect
+    expect(wrapper.find('.el-tag').classes()).toContain('el-tag--light')
+    await (vm.effect = 'plain')
+    expect(wrapper.find('.el-tag').classes()).toContain('el-tag--plain')
   })
 })
