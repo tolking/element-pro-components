0c21606c7900ddb527b2f4aed5cb2801054c3e7e
tolking <qw13131wang@gmail.com>
2021-03-08 11:34:33 +0800

    fix(docs): base not work

--- docs/src/router/index.ts
+++ docs/src/router/index.ts
@@ -11,8 +11,11 @@ import type { IRouteRecordRaw } from '/@src/index'
 const routes: IRouteRecordRaw[] = [...zhCN]
 
 export function createRouter(): Router {
+  const baseUrl = import.meta.env.BASE_URL
   return _createRouter({
-    history: import.meta.env.SSR ? createMemoryHistory() : createWebHistory(),
+    history: import.meta.env.SSR
+      ? createMemoryHistory(baseUrl)
+      : createWebHistory(baseUrl),
     routes: import.meta.env.MODE === 'production' ? routes : routes.concat(dev),
   })
 }
