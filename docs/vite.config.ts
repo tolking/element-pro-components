2541bb99bef30cfd5fd78c19571875df9ec0b7dc
tolking <qw13131wang@gmail.com>
2021-03-08 11:34:33 +0800

    fix(docs): base not work

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import plugins from './src/plugin/common-plugins'
 
 export default defineConfig({
+  base: '/element-pro-components/',
   resolve: {
     alias: {
       '/@src': path.resolve(__dirname, '../src'),
