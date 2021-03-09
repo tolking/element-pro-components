d35bfff01fbdb8b833ecd05d21221fb129a4b38d
tolking <qw13131wang@gmail.com>
2021-03-09 11:03:37 +0800

    feat(docs): add snippet

--- docs/src/plugin/common-plugins.ts
+++ docs/src/plugin/common-plugins.ts
@@ -1,7 +1,8 @@
 import vue from '@vitejs/plugin-vue'
 import Markdown from 'vite-plugin-md'
 import container from 'markdown-it-container'
-import codeSnippet from 'markdown-it-vuepress-code-snippet-enhanced'
+import highlight from './highlight'
+import snippet from './snippet'
 import demo from './demo'
 import createContainer from './create-container'
 import preWrapper from './pre-wrapper'
@@ -12,8 +13,14 @@ const plugins = [
     include: [/\.vue$/, /\.md$/],
   }),
   Markdown({
+    markdownItOptions: {
+      html: true,
+      linkify: true,
+      typographer: true,
+      highlight,
+    },
     markdownItSetup(md) {
-      md.use(codeSnippet)
+      md.use(snippet)
         .use(preWrapper)
         .use(container, 'demo', demo)
         .use(...createContainer('tip', 'TIP'))
@@ -22,6 +29,16 @@ const plugins = [
         .use(container, 'v-pre', {
           render: (tokens: Token[], idx: number) =>
             tokens[idx].nesting === 1 ? '<div v-pre>\n' : '</div>\n',
+        })
+        .use(container, 'details', {
+          render: (tokens: Token[], idx: number) => {
+            const info = tokens[idx].info.trim().slice(7).trim() // 7 = 'details'.length
+            return tokens[idx].nesting === 1
+              ? `<details class="custom-block details">${
+                  info ? `<summary>${info}</summary>` : ''
+                }\n`
+              : '</details>'
+          },
         })
     },
   }),
