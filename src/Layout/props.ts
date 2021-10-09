03d9e044974daba77bbaa47b10ab45b755af09e8
tolking <qw13131wang@gmail.com>
2021-10-09 23:38:43 +0800

    chore(Layout): change props

--- src/Layout/props.ts
+++ src/Layout/props.ts
@@ -1,16 +1,14 @@
 import props from '../Menu/props'
 
-export default Object.assign(
-  {
-    fixedHeader: {
-      type: Boolean,
-      default: true,
-    },
-    fixedMain: {
-      type: Boolean,
-      default: true,
-    },
-    transition: String,
+export default {
+  ...props,
+  fixedHeader: {
+    type: Boolean,
+    default: false,
   },
-  props
-)
+  fixedMain: {
+    type: Boolean,
+    default: false,
+  },
+  transition: String,
+}
