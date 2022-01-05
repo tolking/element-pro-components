35efdac9bfa663b019e6bf56c12750478d51c408
tolking <qw13131wang@gmail.com>
2022-01-05 20:06:15 +0800

    refactor(Form): remove InstallOptions

--- src/Form/index.ts
+++ src/Form/index.ts
@@ -1,25 +1,12 @@
-import { objectDeepMerge } from '../utils/index'
 import ProForm from './Form'
 import props from './props'
-import type {
-  InstallOptions,
-  IDefinePlugin,
-  IDefineProps,
-} from '../types/index'
+import type { IDefinePlugin, IDefineProps } from '../types/index'
 
 const _ProForm: IDefinePlugin<typeof ProForm> = ProForm as IDefinePlugin<
   typeof ProForm
 >
 
-_ProForm.install = (app, options?: InstallOptions) => {
-  if (options) {
-    const _before = app.config.globalProperties.$PROOPTIONS as InstallOptions
-    const _options = _before
-      ? objectDeepMerge<InstallOptions>(_before, options)
-      : options
-    app.config.globalProperties.$PROOPTIONS = _options
-  }
-
+_ProForm.install = (app) => {
   app.component(_ProForm.name, _ProForm)
 }
 
