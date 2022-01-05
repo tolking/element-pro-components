408acbd392e7f4b0f26ceced16f76f2054095ac6
tolking <qw13131wang@gmail.com>
2022-01-05 20:07:06 +0800

    refactor(Crud): remove InstallOptions

--- src/Crud/index.ts
+++ src/Crud/index.ts
@@ -1,25 +1,12 @@
-import { objectDeepMerge } from '../utils/index'
 import ProCrud from './Crud'
 import props from './props'
-import type {
-  InstallOptions,
-  IDefinePlugin,
-  IDefineProps,
-} from '../types/index'
+import type { IDefinePlugin, IDefineProps } from '../types/index'
 
 const _ProCrud: IDefinePlugin<typeof ProCrud> = ProCrud as IDefinePlugin<
   typeof ProCrud
 >
 
-_ProCrud.install = (app, options?: InstallOptions) => {
-  if (options) {
-    const _before = app.config.globalProperties.$PROOPTIONS as InstallOptions
-    const _options = _before
-      ? objectDeepMerge<InstallOptions>(_before, options)
-      : options
-    app.config.globalProperties.$PROOPTIONS = _options
-  }
-
+_ProCrud.install = (app) => {
   app.component(_ProCrud.name, _ProCrud)
 }
 
