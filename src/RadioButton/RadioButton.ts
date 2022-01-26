213c33c011c0fe91ada45955dcd4816d302f30b6
tolking <qw13131wang@gmail.com>
2022-01-26 20:33:06 +0800

    type(RadioButton): optimized the type of emits

--- src/RadioButton/RadioButton.ts
+++ src/RadioButton/RadioButton.ts
@@ -1,12 +1,13 @@
 import { defineComponent } from 'vue'
 import { ElRadioButton } from 'element-plus'
+import { modelValueEmit } from '../utils/index'
 import { createDefault } from '../Radio/Radio'
 import props from '../Radio/props'
 
 export default defineComponent({
   name: 'ProRadioButton',
   props,
-  emits: ['update:modelValue'],
+  emits: modelValueEmit,
   setup(props) {
     return createDefault<typeof ElRadioButton>(
       props,
--- src/RadioButton/index.ts
+++ src/RadioButton/index.ts
@@ -1,6 +1,7 @@
 import { withInstall } from '../utils/index'
 import RadioButton from './RadioButton'
-import type { IRadioProps } from '../Radio/index'
+import type { IRadioProps, IRadioEmits } from '../Radio/index'
 
 export const ProRadioButton = withInstall(RadioButton)
 export type IRadioButtonProps = IRadioProps
+export type IRadioButtonEmits = IRadioEmits
