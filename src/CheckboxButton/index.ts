051d5b4ee73248f59cfbbef3f02d64ed8b102399
tolking <qw13131wang@gmail.com>
2022-01-26 20:22:22 +0800

    type(CheckboxButton): optimized the type of emits

--- src/CheckboxButton/index.ts
+++ src/CheckboxButton/index.ts
@@ -1,6 +1,7 @@
 import { withInstall } from '../utils/index'
 import CheckboxButton from './CheckboxButton'
-import type { ICheckboxProps } from '../Checkbox/index'
+import type { ICheckboxProps, ICheckboxEmits } from '../Checkbox/index'
 
 export const ProCheckboxButton = withInstall(CheckboxButton)
 export type ICheckboxButtonProps = ICheckboxProps
+export type ICheckboxButtonEmits = ICheckboxEmits
