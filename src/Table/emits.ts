3a14c776dbe9ad0822c40f8605cf1b06dd2c6b88
tolking <qw13131wang@gmail.com>
2022-01-29 12:15:17 +0800

    type(Table): optimized the type of emits

---
+++ src/Table/emits.ts
@@ -0,0 +1,8 @@
+export default {
+  'update:currentPage': (current: number) => true,
+  'update:pageSize': (size: number) => true,
+  'size-change': (size: number) => true,
+  'current-change': (current: number) => true,
+  'prev-click': (current: number) => true,
+  'next-click': (current: number) => true,
+}
--- src/Table/index.ts
+++ src/Table/index.ts
@@ -1,7 +1,9 @@
 import { withInstall } from '../utils/index'
 import Table from './Table'
 import props from './props'
-import type { IDefineProps } from '../types/index'
+import emits from './emits'
+import type { IDefineProps, IDefineEmits } from '../types/index'
 
 export const ProTable = withInstall(Table)
 export type ITableProps = IDefineProps<typeof props>
+export type ITableEmits = IDefineEmits<typeof emits>
