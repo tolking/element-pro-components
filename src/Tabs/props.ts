5b80adf49341035bc4c076312b03cd8490fc673f
wang qiang <qw13131wang@gmail.com>
2022-02-16 16:04:09 +0800

    refacto(Tabs): import props from element-plus

--- src/Tabs/props.ts
+++ src/Tabs/props.ts
@@ -1,22 +1,11 @@
-import type { PropType } from 'vue'
+import { tabsProps } from 'element-plus'
+import { objectPick } from '../utils/index'
 
-//TODO: Wait for the internal type reconstruction of the element-plus to try to import from its internal
-type BeforeLeave = (
-  newTabName: string,
-  oldTabName: string
-) => void | Promise<void> | boolean
+const props = objectPick(tabsProps, [
+  'type',
+  'tabPosition',
+  'beforeLeave',
+  'stretch',
+])
 
-export default {
-  type: {
-    type: String as PropType<'card' | 'border-card'>,
-  },
-  tabPosition: {
-    type: String as PropType<'top' | 'right' | 'bottom' | 'left'>,
-    default: 'top',
-  },
-  beforeLeave: {
-    type: Function as PropType<BeforeLeave>,
-    default: null,
-  },
-  stretch: Boolean,
-}
+export default props
