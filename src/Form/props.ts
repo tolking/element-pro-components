c261941324a3ed907e10595860c8abc0165a433e
tolking <qw13131wang@gmail.com>
2022-03-12 14:59:27 +0800

    refacto(Form): rewite props

--- src/Form/props.ts
+++ src/Form/props.ts
@@ -1,52 +1,11 @@
-import { rowProps } from 'element-plus'
+import { formProps, rowProps } from 'element-plus'
 import { objectOmit } from '../utils/index'
 import type { PropType } from 'vue'
-import type { FormRulesMap } from 'element-plus/lib/components/form/src/form.type'
-import type { ComponentSize } from 'element-plus/lib/constants/index'
 import type { IFormColumns, IFormMenuColumns } from './type'
 
 type FormKeys = Array<keyof typeof _formProps>
 
-const _formProps = {
-  rules: {
-    type: Object as PropType<FormRulesMap>,
-  },
-  inline: {
-    type: Boolean,
-    default: false,
-  },
-  labelPosition: {
-    type: String as PropType<'right' | 'left' | 'top'>,
-    default: undefined,
-  },
-  labelWidth: {
-    type: [String, Number],
-    default: '',
-  },
-  labelSuffix: {
-    type: String,
-    default: '',
-  },
-  hideRequiredAsterisk: {
-    type: Boolean,
-    default: false,
-  },
-  showMessage: {
-    type: Boolean,
-    default: true,
-  },
-  inlineMessage: Boolean,
-  statusIcon: Boolean,
-  validateOnRuleChange: {
-    type: Boolean,
-    default: true,
-  },
-  size: {
-    type: String as PropType<ComponentSize>,
-  },
-  disabled: Boolean,
-  scrollToError: Boolean,
-}
+const _formProps = objectOmit(formProps, 'model')
 const _rowProps = objectOmit(rowProps, 'tag')
 
 export const formKeys = Object.keys(_formProps) as FormKeys
