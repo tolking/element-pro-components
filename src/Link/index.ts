54f6833aacf410385a7ab1df94398111dce948be
wang qiang <qw13131wang@gmail.com>
2022-05-31 14:54:42 +0800

    refacto(Link): move the composables function

--- src/Link/index.ts
+++ src/Link/index.ts
@@ -3,5 +3,6 @@ import Link from './Link'
 import props from './props'
 import type { IDefineProps } from '../types/index'
 
+export * from './useLink'
 export const ProLink = withInstall(Link)
 export type ILinkProps = IDefineProps<typeof props>
