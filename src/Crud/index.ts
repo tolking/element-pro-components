1799f6784089f7fd91810c2a0ba81e582ef5a7f5
wang qiang <qw13131wang@gmail.com>
2022-06-01 10:36:15 +0800

    refacto(Crud): move the composables function

--- src/Crud/index.ts
+++ src/Crud/index.ts
@@ -3,6 +3,8 @@ import Crud from './Crud'
 import props from './props'
 import emits from './emits'
 import type { IDefineProps, IDefineEmits } from '../types/index'
+
+export * from './useCrud'
 export * from './type'
 export const ProCrud = withInstall(Crud)
 export type ICrudProps = IDefineProps<typeof props>
--- src/composables/index.ts
+++ src/composables/index.ts
@@ -1,5 +1,4 @@
 export * from './col'
-export * from './crud'
 export * from './data'
 export * from './public'
 export * from './row'
