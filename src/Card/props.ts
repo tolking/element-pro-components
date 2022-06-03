b61505639735743cc89539b6269763e742c137dd
tolking <qw13131wang@gmail.com>
2022-06-03 22:01:11 +0800

    refacto(Card): props

--- src/Card/props.ts
+++ src/Card/props.ts
@@ -1,4 +1,4 @@
-import { cardProps, colProps, rowProps } from 'element-plus'
+import { cardProps as elCardProps, colProps, rowProps } from 'element-plus'
 import { PropType } from 'vue'
 import { objectOmit } from '../utils/index'
 
@@ -7,8 +7,8 @@ type Direction = 'row' | 'row-reverse' | 'column' | 'column-reverse'
 const _rowProps = objectOmit(rowProps, 'tag')
 const _colProps = objectOmit(colProps, 'tag', 'span')
 
-export default {
-  ...cardProps,
+export const cardProps = {
+  ...elCardProps,
   ..._rowProps,
   ..._colProps,
   // Modify the default value to be undefined, which is convenient to determine whether it is a Row
@@ -17,12 +17,6 @@ export default {
     type: String as PropType<Direction>,
     default: 'row',
   },
-  split: {
-    type: Boolean,
-    default: false,
-  },
-  ghost: {
-    type: Boolean,
-    default: false,
-  },
+  split: Boolean,
+  ghost: Boolean,
 }
