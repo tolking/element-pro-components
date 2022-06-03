c08fa5c64d10134434a946bb0bdf23cb763d872b
tolking <qw13131wang@gmail.com>
2022-06-03 22:23:09 +0800

    refacto(RadioButton): props

--- src/RadioButton/RadioButton.ts
+++ src/RadioButton/RadioButton.ts
@@ -2,11 +2,11 @@ import { defineComponent } from 'vue'
 import { ElRadioButton } from 'element-plus'
 import { modelValueEmit } from '../utils/index'
 import { createDefault } from '../Radio/Radio'
-import props from '../Radio/props'
+import { radioProps } from '../Radio/props'
 
 export default defineComponent({
   name: 'ProRadioButton',
-  props,
+  props: radioProps,
   emits: modelValueEmit,
   setup(props) {
     return createDefault<typeof ElRadioButton>(
--- src/RadioButton/index.ts
+++ src/RadioButton/index.ts
@@ -2,6 +2,7 @@ import { withInstall } from '../utils/index'
 import RadioButton from './RadioButton'
 import type { IRadioProps, IRadioEmits } from '../Radio/index'
 
+export { radioProps as radioButtonProps } from '../Radio/props'
 export const ProRadioButton = withInstall(RadioButton)
 export type IRadioButtonProps = IRadioProps
 export type IRadioButtonEmits = IRadioEmits
