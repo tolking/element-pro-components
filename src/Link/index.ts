4746c97bcd12d620eb8b348fe59007919f712f08
tolking <qw13131wang@gmail.com>
2022-06-04 15:28:43 +0800

    refacto(Link): props

--- src/Link/index.ts
+++ src/Link/index.ts
@@ -1,8 +1,8 @@
 import { withInstall } from '../utils/index'
 import Link from './Link'
-import props from './props'
+import { linkProps } from './props'
 import type { IDefineProps } from '../types/index'
 
-export * from './useLink'
+export { linkProps } from './props'
 export const ProLink = withInstall(Link)
-export type ILinkProps = IDefineProps<typeof props>
+export type ILinkProps = IDefineProps<typeof linkProps>
