dc33af946a0f80e6e39712a64c181ff4140db710
tolking <qw13131wang@gmail.com>
2022-06-05 10:14:13 +0800

    refacto(TreeSelect): emits

--- src/TreeSelect/index.ts
+++ src/TreeSelect/index.ts
@@ -1,10 +1,9 @@
 import { withInstall } from '../utils/index'
 import TreeSelect from './TreeSelect'
-import { treeSelectProps } from './props'
-import emits from './emits'
+import { treeSelectProps, treeSelectEmits } from './props'
 import type { IDefineProps, IDefineEmits } from '../types/index'
 
-export { treeSelectProps } from './props'
+export { treeSelectProps, treeSelectEmits } from './props'
 export const ProTreeSelect = withInstall(TreeSelect)
 export type ITreeSelectProps = IDefineProps<typeof treeSelectProps>
-export type ITreeSelectEmits = IDefineEmits<typeof emits>
+export type ITreeSelectEmits = IDefineEmits<typeof treeSelectEmits>
