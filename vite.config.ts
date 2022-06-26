32d6207f810882ed0c5e26a1caaab207335977cc
tolking <qw13131wang@gmail.com>
2022-06-26 20:03:13 +0800

    chore: update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import Markdown from 'vite-plugin-md'
+import Markdown from 'vite-plugin-vue-markdown'
 import container from 'markdown-it-container'
 import { VitePWA } from 'vite-plugin-pwa'
 import anchor from 'markdown-it-anchor'
