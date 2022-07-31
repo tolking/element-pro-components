7a3cb7b6052b6fc96c2ee4e1328f2a900ea55103
tolking <qw13131wang@gmail.com>
2022-07-31 10:48:42 +0800

    dome(Tabs): add demo

---
+++ demo/Tabs/before-add.vue
@@ -0,0 +1,25 @@
+<template>
+  <pro-tabs :before-add="beforeAdd" />
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { ElNotification } from 'element-plus'
+
+export default defineComponent({
+  setup() {
+    function beforeAdd(e) {
+      console.log('Tabs-before-add', e)
+      ElNotification({
+        type: 'success',
+        title: 'Success',
+        message: 'Come from before-add',
+      })
+    }
+
+    return {
+      beforeAdd,
+    }
+  },
+})
+</script>
