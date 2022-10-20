32780a2fd5ff9b942d96dac0c8ebd5516accc7aa
tolking <qw13131wang@gmail.com>
2022-10-20 19:01:52 +0800

    demo(ArrayForm): add demo

---
+++ demo/ArrayForm/async.vue
@@ -0,0 +1,88 @@
+<template>
+  <div style="margin-bottom: 20px">
+    <el-button
+      type="primary"
+      @click="createForm"
+    >
+      Load Form
+    </el-button>
+    <el-button
+      type="info"
+      @click="createDict"
+    >
+      Load Dict
+    </el-button>
+    <el-button
+      type="danger"
+      @click="destroyForm"
+    >
+      Destroy
+    </el-button>
+  </div>
+  <pro-array-form
+    v-model="form"
+    :columns="columns"
+    label-width="100px"
+    @submit="submit"
+  />
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import { ElMessage } from 'element-plus'
+import { defineFormColumns, defineFormSubmit } from 'element-pro-components'
+
+export default defineComponent({
+  setup() {
+    const list = ref([])
+    const form = ref([{}])
+    const columns = ref(defineFormColumns([]))
+    const submit = defineFormSubmit((done, isValid, invalidFields) => {
+      ElMessage(`submit: ${isValid}`)
+      console.log(form.value, isValid, invalidFields)
+      setTimeout(() => {
+        done()
+      }, 1000)
+    })
+    const createForm = () => {
+      columns.value = defineFormColumns([
+        {
+          label: 'Name',
+          prop: 'name',
+          component: 'el-input',
+        },
+        {
+          label: 'Language',
+          prop: 'language',
+          component: 'pro-radio',
+          props: {
+            data: list,
+          },
+        },
+      ])
+    }
+    const createDict = () => {
+      list.value = [
+        { value: 'Go', label: 'go' },
+        { value: 'JavaScript', label: 'javascript' },
+        { value: 'Python', label: 'python' },
+        { value: 'Dart', label: 'dart' },
+        { value: 'V', label: 'v' },
+      ]
+    }
+    const destroyForm = () => {
+      columns.value = []
+      list.value = []
+    }
+
+    return {
+      form,
+      columns,
+      submit,
+      createForm,
+      createDict,
+      destroyForm,
+    }
+  },
+})
+</script>
---
+++ demo/ArrayForm/component.vue
@@ -0,0 +1,88 @@
+<template>
+  <pro-array-form
+    v-model="form"
+    :columns="columns"
+    label-width="100px"
+    @submit="submit"
+  />
+</template>
+
+<script>
+import { defineComponent, markRaw, ref } from 'vue'
+import { ElMessage } from 'element-plus'
+import { Search } from '@element-plus/icons-vue'
+import { defineFormColumns, defineFormSubmit } from 'element-pro-components'
+
+export default defineComponent({
+  setup() {
+    const form = ref([{}])
+    const list = ref([
+      { value: 'Go', label: 'go' },
+      { value: 'JavaScript', label: 'javascript' },
+      { value: 'Python', label: 'python' },
+      { value: 'Dart', label: 'dart' },
+      { value: 'V', label: 'v' },
+    ])
+    const columns = defineFormColumns([
+      {
+        label: 'input',
+        prop: 'input',
+        component: 'el-input',
+        props: {
+          clearable: true,
+          placeholder: 'Please input',
+          prefixIcon: markRaw(Search),
+          slots: {
+            append: () => 'Search',
+          },
+        },
+      },
+      {
+        label: 'input-tag',
+        prop: 'inputTag',
+        component: 'pro-input-tag',
+        props: {
+          placeholder: 'Please click the space button after input',
+        },
+      },
+      {
+        label: 'radio',
+        prop: 'radio',
+        component: 'pro-radio',
+        props: {
+          data: list.value,
+        },
+      },
+      {
+        label: 'checkbox',
+        prop: 'checkbox',
+        component: 'pro-checkbox',
+        props: {
+          data: list.value,
+        },
+      },
+      {
+        label: 'select',
+        prop: 'select',
+        component: 'pro-select',
+        props: {
+          data: list.value,
+        },
+      },
+    ])
+    const submit = defineFormSubmit((done, isValid, invalidFields) => {
+      ElMessage(`submit: ${isValid}`)
+      console.log(form.value, isValid, invalidFields)
+      setTimeout(() => {
+        done()
+      }, 1000)
+    })
+
+    return {
+      form,
+      columns,
+      submit,
+    }
+  },
+})
+</script>
