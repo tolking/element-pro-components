f0dfada62dd8937db5b33657f557cacc9e251f13
tolking <qw13131wang@gmail.com>
2022-10-26 16:06:10 +0800

    demo(Menu): add demo

---
+++ demo/Menu/group.vue
@@ -0,0 +1,53 @@
+<template>
+  <pro-menu
+    :routes="routes"
+    class="docs-menu"
+  />
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const routes = [
+      {
+        path: '/one',
+        component: { template: 'one page' },
+        meta: { title: 'one', group: true },
+        children: [
+          {
+            path: '/one/index',
+            component: { template: 'one index page' },
+            meta: { title: 'oneIndex' },
+          },
+          {
+            path: '/one/info',
+            component: { template: 'one info page' },
+            meta: { title: 'oneInfo' },
+          },
+        ],
+      },
+      {
+        path: '/two',
+        component: { template: 'two page' },
+        meta: { title: 'two', group: true },
+        children: [
+          {
+            path: '/two/index',
+            component: { template: 'two page' },
+            meta: { title: 'twoIndex' },
+          },
+          {
+            path: '/two/info',
+            component: { template: 'two page' },
+            meta: { title: 'twoInfo' },
+          },
+        ],
+      },
+    ]
+
+    return { routes }
+  },
+})
+</script>
